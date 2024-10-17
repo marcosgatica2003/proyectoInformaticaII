e78a0f0118abd2087924b570809ec1780bf60348
marcosgatica2003 <marcosgatica1112@gmail.com>
2024-10-17 12:16:09 -0300

    Configuración de Doxyfile & Script para generar y mostrar documentación hecho

---
+++ software/generarDocumentacionDeCodigos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DOXYFILE_DIR="."
+DOCS_DIR="$DOXYFILE_DIR/documentacionCodigos"
+
+mkdir -p "$DOCS_DIR"
+
+doxygen "$DOXYFILE_DIR/Doxyfile"
+
+cd "$DOXYFILE_DIR/documentacionCodigos/latex" || exit
+
+make
+
+mv refman.pdf S.S.C.F.Software.pdf
+mv S.S.C.F.Software.pdf ../
+
+cd - || exit
+
+echo "Documentación de software generada como S.S.C.F.Software.pdf"
