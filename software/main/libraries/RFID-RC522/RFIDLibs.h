1c7303dcf51fa66b668f3bde3230437701865abb
marcosgatica2003 <marcosgatica1112@gmail.com>
2024-10-20 01:26:47 -0300

    Código para RFID-RC522 terminado

---
+++ software/main/libraries/RFID-RC522/RFIDLibs.h
@@ -0,0 +1,17 @@
+#ifndef RFIDLIBS_H
+#define RFIDLIBS_H
+
+#include "accesoRFID.h"
+#include "accesoRFID.cpp"
+
+#include "deprecated.h"
+
+#include "MFRC522.h"
+#include "MFRC522.cpp"
+
+#include "MFRC522Extended.h"
+#include "MFRC522Extended.cpp"
+
+#include "require_cpp11.h"
+
+#endif
